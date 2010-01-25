1bf7bc9ad7795e1832ab8b45cf932fb31139abe3
gordonw <gordon.williams@collabora.co.uk>
2010-01-25 14:24:38 +0000

    0.8.2-0maemo64

--- clutter/clutter-timeline.c
+++ clutter/clutter-timeline.c
@@ -94,6 +94,7 @@
 G_DEFINE_TYPE (ClutterTimeline, clutter_timeline, G_TYPE_OBJECT);
 
 #define FPS_TO_INTERVAL(f) (1000 / (f))
+#define CLUTTER_TIMELINE_DEFAULT_FPS (clutter_get_default_frame_rate())
 
 struct _ClutterTimelinePrivate
 {
@@ -113,10 +114,6 @@ struct _ClutterTimelinePrivate
   GTimeVal prev_frame_timeval;
   guint  msecs_delta;
   gint   msecs_jitter; /* To allow us to render a frame a little early/late */
-  gint   msecs_avr; /* Average delay between frames - we have to average as the
-                       SGX pipeline can store many frames. This starts off as 0,
-                       which helps to negate the initial delay as the frame works
-                       its way through the pipeline */
 
   GHashTable *markers_by_frame;
   GHashTable *markers_by_name;
@@ -378,7 +375,7 @@ clutter_timeline_class_init (ClutterTimelineClass *klass)
                                                       "Frames Per Second",
                                                       "Timeline frames per second",
                                                       1, 1000,
-                                                      60,
+                                                      CLUTTER_TIMELINE_DEFAULT_FPS,
                                                       CLUTTER_PARAM_READWRITE));
   /**
    * ClutterTimeline:num-frames:
@@ -570,11 +567,10 @@ clutter_timeline_init (ClutterTimeline *self)
                                                    CLUTTER_TYPE_TIMELINE,
                                                    ClutterTimelinePrivate);
 
-  priv->fps = clutter_get_default_frame_rate ();
+  priv->fps = CLUTTER_TIMELINE_DEFAULT_FPS;
   priv->n_frames = 0;
   priv->msecs_delta = 0;
   priv->msecs_jitter = 0;
-  priv->msecs_avr = 0;
 
   priv->markers_by_frame = g_hash_table_new (NULL, NULL);
   priv->markers_by_name = g_hash_table_new_full (g_str_hash, g_str_equal,
@@ -615,44 +611,20 @@ timeline_timeout_func (gpointer data)
   /* Interpolate the current frame based on the timeval of the
    * previous frame. Also use msecs_jitter - the sum of all errors
    * since we started playing. This means we won't play things
-   * too fast/slow if we repeatedly under/overestimate the frame time.
-   *
-   * Since SGX queues stuff up and takes a while to render, the time difference
-   * between calls to this function is NOT the same as the time difference
-   * between frames as they appear on the screen. We average the frame time
-   * here to roughly try simulate this as we have no feedback.
-   *
-   * My take on the lurching that we are trying to fix... it might be wrong,
-   * but it's been run past the SGX guys and they think it's fair:
-   *
-   * Imagine we send frames at roughly constant intervals of 30ms
-   * SGX/CPU takes a while to power up, but once it's done it can render in
-   * maybe 15ms, So :
-   *  * maybe 70ms for first frame (during this time some frames queue up)
-   *  * then 15ms, 15ms, 15ms (as queue is emptied)
-   *  * then 30ms as queue is mostly empty and stuff settles down
-   *
-   * So we have to deal with this - which is why we use msecs_avr and
-   * start it at 0 so it (VERY roughly) matches the first few frames...
-   *
-   *    15, 22, 26, 28, 29, 30, 30, 30...
-   *
-   *  */
+   * too fast/slow if we repeatedly under/overestimate the frame time. */
+
   msecs = (timeval.tv_sec - priv->prev_frame_timeval.tv_sec) * 1000;
   msecs += (timeval.tv_usec - priv->prev_frame_timeval.tv_usec) / 1000;
-  priv->msecs_avr = (priv->msecs_avr*3+msecs) / 4;
-  priv->msecs_jitter += priv->msecs_avr;
+  priv->msecs_jitter += msecs;
   if (context->disable_skip_frames == FALSE)
     {
-      gint frame_ms = 1000 / priv->fps;
+      gint frame_ms = FPS_TO_INTERVAL(priv->fps);
       n_frames = priv->msecs_jitter / frame_ms;
       if (n_frames <= 0)
         n_frames = 1;
 
       priv->msecs_jitter -= n_frames * frame_ms;
       priv->skipped_frames = n_frames - 1;
-
-      //g_debug("JITTER %d (%d frames, %d ms, %d avr ms)", priv->msecs_jitter, n_frames, msecs, priv->msecs_avr);
     }
   else
     {
